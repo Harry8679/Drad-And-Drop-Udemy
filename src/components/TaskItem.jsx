db73eb808c8a068a526c20b2f74dd39f04035d64
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-09 14:20:27 +0100

    Task Item 2

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { Draggable } from "react-beautiful-dnd";
 
-export default function TaskItem({ task, index }) {
+export default function TaskItem({ task }) {
   return (
-    <Draggable draggableId={task.id} index={index}>
-      {(provided) => (
-        <div
-          className="bg-white p-2 mb-2 rounded shadow"
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-        >
-          {task.content}
-        </div>
-      )}
-    </Draggable>
+    <div className="p-2 mb-2 bg-white rounded shadow cursor-pointer">
+      {task.content}
+    </div>
   );
 }
