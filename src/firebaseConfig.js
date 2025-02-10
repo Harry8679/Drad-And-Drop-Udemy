e07a9f0e66ecdaa8aef7057da27eadea3cb739d5
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-10 02:23:54 +0100

    Firebase Config fillfuled

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_PROJECT.appspot.com",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyCppJuOt-on1Y23uXpSNJg3HgPoDxTylsE",
+  authDomain: "gestionnairetaches-dc64b.firebaseapp.com",
+  projectId: "gestionnairetaches-dc64b",
+  storageBucket: "gestionnairetaches-dc64b.firebasestorage.app",
+  messagingSenderId: "523538599407",
+  appId: "1:523538599407:web:34f11a34737dca3296ba1a",
+  measurementId: "G-H5QY7K4N2L"
 };
+// const firebaseConfig = {
+//   apiKey: "TON_API_KEY",
+//   authDomain: "TON_PROJET.firebaseapp.com",
+//   projectId: "TON_PROJECT_ID",
+//   storageBucket: "TON_PROJECT.appspot.com",
+//   messagingSenderId: "TON_SENDER_ID",
+//   appId: "TON_APP_ID"
+// };
 
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
